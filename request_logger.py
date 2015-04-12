aa85517b2a7602fdf6338dc6a773b3bab7532359
minniek <minniek@bu.edu>
2015-04-12 17:02:44 -0400

    added comments

--- request_logger.py
+++ request_logger.py
@@ -1,3 +1,10 @@
+'''
+Middleware that logs all requests
+
+References:
+[1] http://stackoverflow.com/questions/4839297/is-there-a-django-middleware-plugin-that-logs-all-my-requests-in-a-organized-fas
+'''
+
 class RequestLogger(object):
 
 	def process_request(self, request):
