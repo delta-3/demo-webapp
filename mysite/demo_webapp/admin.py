def5bf45e829e6d4be89236af1618cd70714f933
dddotcom <dddotcom@gmail.com>
2015-04-16 16:42:19 -0400

    ignoring pyc files, added sortby full url in admin view

--- mysite/demo_webapp/admin.py
+++ mysite/demo_webapp/admin.py
@@ -5,7 +5,7 @@ from .models import Request
 
 class RequestAdmin(admin.ModelAdmin):
 	list_display = ('timestamp', 'full_url', 'is_good', 'param_map')
-	list_filter = ['is_good', 'timestamp']
+	list_filter = ['is_good'. 'full_url']
 
 admin.site.register(Request, RequestAdmin)
 
