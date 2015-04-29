b61c403f5b47581f214b39f3e0b8173b4da0ee97
dddotcom <dddotcom@gmail.com>
2015-04-29 09:19:59 -0400

    updated database to include comments model and new attributes for user model

--- delta3/models.py
+++ delta3/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class User(models.Model):
 	username = models.CharField(max_length=35)
 	password = models.CharField(max_length=200)
+	firstname = models.CharField(max_length=35)
+	lastname = models.CharField(max_length=35)
+	age = models.CharField(max_length=3)
 
 	def __unicode__(self):
 		return "username :" + str(self.username) + ", password : " + str(self.password)
