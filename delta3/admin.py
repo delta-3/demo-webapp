c1aa1c8ee83d8d61a0bb7e356cced4b531d3aeab
minniek <minniek@bu.edu>
2015-04-28 22:40:18 -0400

    added Comment Admin model

--- delta3/admin.py
+++ delta3/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Gif, User
+from .models import Gif, User, Comment
 
 class GifAdmin(admin.ModelAdmin):
 	list_display = ('gif_name', 'gif_url')
@@ -11,3 +11,8 @@ class UserAdmin(admin.ModelAdmin):
 	list_display = ('username', 'password')
 
 admin.site.register(User, UserAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+	#list_display = ('content')
+	pass
+admin.site.register(Comment, CommentAdmin)
